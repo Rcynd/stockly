552591c8912f4127da485f3ddecdad0d30438cf7
Rian Muhammad Afriansyah <rianma1333@gmail.com>
2023-05-24 09:59:33 +0700

    membuat model produk

--- WebApp-bootstrap/app/Models/Produk.php
+++ WebApp-bootstrap/app/Models/Produk.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Produk extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+
+    public function scopeFilter($query, array $filters){
+        $query->when($filters['search'] ?? false, function($query, $search){
+            return $query->whereHas('jenis', function($query) use ($search){
+                $query->where('name_jenis','like','%' . $search . '%')
+                ->orWhere('bruto','like','%' . $search . '%')
+                ;
+            })->orWhere('name_produk','like','%' . $search . '%')
+            ->orWhere('status','like','%' . $search . '%')
+            ->orWhere('stock','like', $search )
+            ->orWhere('harga','like','%' . $search . '%')
+            ;
+        });
+    }
 }
