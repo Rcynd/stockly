a445eae1c14ddddbf6810749a882cd838acabfae
Rian Muhammad Afriansyah <rianma1333@gmail.com>
2023-05-24 10:00:43 +0700

    membuat produk controller dan dan menambahkan fungsi untuk tambah produk

--- WebApp-bootstrap/app/Http/Controllers/ProdukController.php
+++ WebApp-bootstrap/app/Http/Controllers/ProdukController.php
@@ -3,10 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Produk;
 class ProdukController extends Controller
 {
     public function index(){
-        return view('admin.produk.index');
+        return view('admin.produk.index',[
+            'produks' => Produk::latest()->filter(request(['search']))->paginate(6)->withQueryString(),
+        ]);
+    }
+
+    public function tambah(){
+        return view('admin.produk.tambah');
+    }
+    public function store(Request $request){
+        $validateData = $request->validate([
+            'name_produk' => 'required',
+            'harga' => 'required',
+            'stock' => 'required',
+            'status' => 'required',
+            'image' => 'image|file|max:20000 ',
+        ]);
+        $validateData['tgl_pengaduan'] = now();
+
+        if ($request->file('image')) {
+            $validateData['image'] = $request->file('image')->store('produk-images');
+        }
+
+        Produk::create($validateData);
+
+        return redirect('/produk-adm')->with('sukses', 'Produk berhasil ditambahkan');
+
     }
 }
